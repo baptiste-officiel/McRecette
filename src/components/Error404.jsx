794d9cc7082d05296ccc453a8445c60136b9903f
Baptiste <baptiste.lejeune@hotmail.com>
2023-05-08 16:48:34 +0200

    👌 IMPROVE: french to english

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -6,7 +6,7 @@ function Error404() {
     <Error>
       <img src={error404} alt="404 error" />
       <h2>404 ERROR</h2>
-      <p>La page que vous demandez n'est pas disponible</p>
+      <p>The content you ask for is not available</p>
       <MainPageLink to={'/'}>Go back to main page</MainPageLink>
     </Error>
   )
